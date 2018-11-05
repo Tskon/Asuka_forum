ca53e6c9195b8f230030fc0a060469ef70e9b776
Tskon <tsekhmeister.k@gmail.com>
2018-11-05 15:09:25 +0300

    change webpack path to wordpress theme

--- webpack.config.js
+++ webpack.config.js
@@ -1,78 +1,12 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const CopyWebpackPlugin = require('copy-webpack-plugin');
+/*
+Theme Name: Asuka
+Author: BeatxD
+Description: Theme for Asuka`s site
+License: GNU General Public License v2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+Tags: blog, two-columns, left-sidebar, accessibility-ready, custom-background, custom-colors, custom-header, custom-logo, custom-menu, editor-style, featured-images, microformats, post-formats, rtl-language-support, sticky-post, threaded-comments, translation-ready
+Text Domain: asuka
 
-module.exports = {
-    entry: {
-        main: './src/main.js',
-    },
-    output: {
-        filename: 'js/[name].js',
-        path: path.resolve(__dirname, 'public'),
-        // publicPath: './'
-    },
-    resolve: {
-        modules: [ path.resolve(__dirname, "src"), "node_modules" ]
-    },
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(png|svg|gif|jpe?g)$/,
-                use: [
-                    'file-loader?name=img/[name].[ext]'
-                ]
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: {publicPath: '../'}
-                    },
-                    'css-loader',
-                    {
-                        loader: 'postcss-loader',
-                        options: {
-                            plugins: () => [ require('autoprefixer')({
-                                'browsers': [ '> 1%', 'last 2 versions' ]
-                            }) ],
-                        }
-                    },
-                    'sass-loader'
-                ]
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                }
-            }
-        ]
-    },
-    plugins: [
-        // new CleanWebpackPlugin([ 'public' ]),
-        new CopyWebpackPlugin([ { from: 'src/img/', to: 'img/',toType: 'dir'} ], {/*options*/}),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            filename: 'index.html',
-        }),
-        new MiniCssExtractPlugin({
-            filename: "css/[name].css",
-            chunkFilename: "css/[id].css"
-        })
-    ],
-    optimization:
-        {
-            minimizer: [ new OptimizeCSSAssetsPlugin({}) ]
-        }
-    ,
-    stats: {
-        colors: true,
-        chunks: true
-    }
-};
+This theme, like WordPress, is licensed under the GPL.
+Use it to make something cool, have fun, and share what you've learned with others.
+*/
