2f11ad8542075f848651ed2305d89361fb70f231
Tskon <tsekhmeister.k@gmail.com>
2018-11-05 15:51:39 +0300

    add main files content

--- public_html/wp-content/themes/asuka/footer.php
+++ public_html/wp-content/themes/asuka/footer.php
@@ -1,7 +1,3 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tskon
- * Date: 05.11.2018
- * Time: 15:09
- */
+</main>
+</body>
+</html>
--- public_html/wp-content/themes/asuka/header.php
+++ public_html/wp-content/themes/asuka/header.php
@@ -1,7 +1,23 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Tskon
- * Date: 05.11.2018
- * Time: 15:10
- */
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover">
+  <meta charset="UTF-8">
+  <title>Title</title>
+</head>
+<body>
+<div class="headerWrapper">
+  <header class="header">
+    <div class="logo">
+      <div class="logo__img">&nbsp;</div>
+      <span class="logo__title">Asuka</span></div>
+    <nav class="mainMenu">
+      <ul>
+        <li class="active"><a href="#">Главная</a></li>
+        <li><a href="#">Вербовочный пункт</a></li>
+        <li><a href="#">Форум</a></li>
+      </ul>
+    </nav>
+  </header>
+</div>
+<main class="indexPage">
