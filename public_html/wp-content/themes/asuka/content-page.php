503c3cdee00be8db5e81c37d5a5faa355b6c1eca
Tskon <tsekhmeister.k@gmail.com>
2018-11-21 00:14:09 +0300

    add content-page

---
+++ public_html/wp-content/themes/asuka/content-page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The template used for displaying page content
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fifteen
+ * @since Twenty Fifteen 1.0
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header>
+
+	<div class="entry-content">
+		<?php the_content(); ?>
+		<?php
+			wp_link_pages( array(
+				'before'      => '<div class="page-links"><span class="page-links-title">' .'Страницы:' . '</span>',
+				'after'       => '</div>',
+				'link_before' => '<span>',
+				'link_after'  => '</span>',
+				'pagelink'    => '<span class="pagelink">' . 'Страница' . ' </span>%',
+				'separator'   => '<span class="separator">, </span>',
+			) );
+		?>
+	</div>
+
+	<?php edit_post_link( 'Редактировать', '<div class="edit-link">', '</div>' ); ?>
+
+</article>
