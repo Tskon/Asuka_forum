f8a8fd87f18075c1d00afd92460f76078d063bf9
Tskon <tsekhmeister.k@gmail.com>
2018-11-21 01:53:28 +0300

    add content-search.php

---
+++ public_html/wp-content/themes/asuka/content-search.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * The template part for displaying results in search pages
+ * Learn more: {@link https://codex.wordpress.org/Template_Hierarchy}
+ */
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php twentyfifteen_post_thumbnail(); ?>
+
+	<header class="entry-header">
+		<?php the_title( sprintf( '<h2><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' ); ?>
+	</header>
+
+	<div>
+		<?php the_excerpt(); ?>
+	</div>
+	<?php edit_post_link( 'Редактировать', '<span>', '</span>' ); ?>
+</article><!-- #post-## -->
