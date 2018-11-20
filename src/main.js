1e5aee7043b758fa45db2f2e0c803c152e6defb4
tskon <tsekhmeister.k@gmail.com>
2018-11-20 21:19:23 +0300

    add slider module

--- src/main.js
+++ src/main.js
@@ -1,10 +1,2 @@
 import 'scss/main.scss';
-
-const test = ()=>{
-    arr = [1, 2, {some: 111}];
-    arr2 = [...arr];
-    console.log(arr2)
-}
-
-const some = 'sdssd';
-const some2 = `sss ${some} sss`;
+import 'vueModules/slider';
