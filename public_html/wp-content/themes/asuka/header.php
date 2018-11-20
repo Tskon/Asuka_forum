9d24832b1ba783808c9de280be730fd885206895
Tskon <tsekhmeister.k@gmail.com>
2018-11-21 02:29:26 +0300

    change styles

--- public_html/wp-content/themes/asuka/header.php
+++ public_html/wp-content/themes/asuka/header.php
@@ -2,24 +2,27 @@
 <html <?php language_attributes(); ?>>
 <head>
   <meta name="viewport" content="width=device-width,initial-scale=1,viewport-fit=cover">
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta charset="<?php bloginfo('charset'); ?>">
   <link rel="stylesheet" href="<?php bloginfo('template_url'); ?>/css/main.css">
   <?php wp_head(); ?>
 </head>
 <body>
 <div class="headerWrapper">
   <header class="header">
-    <div class="logo">
+
+    <a class="logo" href="/">
       <div class="logo__img">&nbsp</div>
-      <span class="logo__title">Asuka</span></div>
-    <?php if ( has_nav_menu( 'header_menu' ) ) : ?>
+      <span class="logo__title">Asuka</span>
+    </a>
+
+    <?php if (has_nav_menu('header_menu')) : ?>
       <nav class="mainMenu" role="navigation">
         <?php
-        wp_nav_menu( array(
+        wp_nav_menu(array(
           'theme_location' => 'header_menu',
-          'menu_class'     => 'ssssss',
-          'depth'          => 1,
-        ) );
+          'menu_class' => '',
+          'depth' => 1,
+        ));
         ?>
       </nav>
     <?php endif; ?>
