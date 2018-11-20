60c20b7f0c7650cf0bb4c064275b86c3294caf9c
Tskon <tsekhmeister.k@gmail.com>
2018-11-21 01:38:34 +0300

    add 404.php

---
+++ public_html/wp-content/themes/asuka/404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+<main>
+  <section>
+    <header>
+      <h1>Страница не найдена</h1>
+    </header>
+    <div>
+      <p>По данному адресу ничего не найдено, возможно поможет поиск:</p>
+      <?php get_search_form(); ?>
+    </div>
+  </section>
+</main>
+<?php get_footer(); ?>
