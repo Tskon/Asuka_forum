325c6dcce05dd8abc3a6edae42df2517e150aca4
Tskon <tsekhmeister.k@gmail.com>
2018-11-21 18:36:47 +0300

    burger fix

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,16 +1,16 @@
 (function () {
   const burgerBtn = document.querySelector('.mainMenu__burgerBtn');
-  const menu = document.querySelector('.menu-main-container');
+  const menu = document.querySelector('.menu-main-menu-container');
 
   document.body.addEventListener('click', e => {
-    if (menu.classList.contains('menu-main-container--show')) {
-      menu.classList.remove('menu-main-container--show');
+    if (menu.classList.contains('menu-main-menu-container--show')) {
+      menu.classList.remove('menu-main-menu-container--show');
     }
   });
 
   burgerBtn.addEventListener('click', e => {
     setTimeout(() => {
-      menu.classList.toggle('menu-main-container--show');
+      menu.classList.toggle('menu-main-menu-container--show');
     }, 0);
   })
 
