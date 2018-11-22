2c129382f923205ee164a6f803654f25356b77f4
Tskon <tsekhmeister.k@gmail.com>
2018-11-22 19:48:43 +0300

    few changes

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,17 +1,18 @@
 (function () {
-  const burgerBtn = document.querySelector('.mainMenu__burgerBtn');
-  const menu = document.querySelector('.menu-main-menu-container');
+  setTimeout(()=>{
+    const burgerBtn = document.querySelector('.mainMenu__burgerBtn');
+    const menu = document.querySelector('.menu-main-menu-container');
 
-  document.body.addEventListener('click', e => {
-    if (menu.classList.contains('menu-main-menu-container--show')) {
-      menu.classList.remove('menu-main-menu-container--show');
-    }
-  });
+    document.querySelector('.home').addEventListener('click', e => {
+      if (menu.classList.contains('menu-main-menu-container--show')) {
+        menu.classList.remove('menu-main-menu-container--show');
+      }
+    });
 
-  burgerBtn.addEventListener('click', e => {
-    setTimeout(() => {
-      menu.classList.toggle('menu-main-menu-container--show');
-    }, 0);
-  })
-
+    burgerBtn.addEventListener('click', e => {
+      setTimeout(() => {
+        menu.classList.toggle('menu-main-menu-container--show');
+      }, 0);
+    })
+  },100);
 }());
