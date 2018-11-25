717dcb22aaf2cafa5448c78042ad69590358a67f
Tskon <tsekhmeister.k@gmail.com>
2018-11-25 13:40:51 +0300

    git ignore

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,18 +1,13 @@
 (function () {
-  setTimeout(()=>{
+  setTimeout(() => {
     const burgerBtn = document.querySelector('.mainMenu__burgerBtn');
     const menu = document.querySelector('.menu-main-menu-container');
 
-    document.querySelector('.home').addEventListener('click', e => {
-      if (menu.classList.contains('menu-main-menu-container--show')) {
-        menu.classList.remove('menu-main-menu-container--show');
-      }
-    });
-
-    burgerBtn.addEventListener('click', e => {
-      setTimeout(() => {
+    if (burgerBtn && menu){
+      burgerBtn.addEventListener('click', e => {
         menu.classList.toggle('menu-main-menu-container--show');
-      }, 0);
-    })
-  },100);
+      })
+    }
+
+  }, 100);
 }());
